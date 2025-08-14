a6e6898e2f6f04de6dcfb343b667490c28284145
somshekhar swami <someshekarswami369@gmail.com>
2025-08-14 09:55:53 +0530

    fixed incorrect api imports

--- varahi-frontend/src/Pages/Pants.jsx
+++ varahi-frontend/src/Pages/Pants.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from "api";
+import api from "../api/api";
 import { useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
 import ProductCardUI from "../Components/ProductCardUI";
